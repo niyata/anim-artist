81c41c0610bf3af0d39046d9a1de9e2e38e04ce9
niyata <niyata2206@gmail.com>
2018-12-09 18:04:59 +0700

    change string "permalink to" => " เปิดไปที่"

--- template-parts/loop-caption.php
+++ template-parts/loop-caption.php
@@ -7,7 +7,7 @@
 	<article id="post-<?php the_ID(); ?>" <?php post_class('seed-col'); ?>>
 		<div class="content-item -caption">
 			<div class="pic">
-				<a href="<?php the_permalink(); ?>" title="Permalink to <?php the_title_attribute(); ?>" rel="bookmark">
+				<a href="<?php the_permalink(); ?>" title="เปิดไปที่ <?php the_title_attribute(); ?>" rel="bookmark">
 					<?php if(has_post_thumbnail()) { the_post_thumbnail();} else { echo '<img src="' . esc_url( get_template_directory_uri()) .'/img/thumb.jpg" alt="'. get_the_title() .'" />'; }?>
 				</a>
 			</div><!--pic-->
